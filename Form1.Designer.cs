8bf0ea98e2e6c35e090ab9820c3b69c20c0e086f
Phisict <adrianfathan69@gmail.com>
2025-05-09 14:40:50 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WinformNPGSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			gridTodo = new DataGridView();
+			boxTodo = new TextBox();
+			contextMenuStrip1 = new ContextMenuStrip(components);
+			buttonSimpan = new Button();
+			((System.ComponentModel.ISupportInitialize)gridTodo).BeginInit();
+			SuspendLayout();
+			// 
+			// gridTodo
+			// 
+			gridTodo.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			gridTodo.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+			gridTodo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			gridTodo.Location = new Point(12, 12);
+			gridTodo.Name = "gridTodo";
+			gridTodo.RowHeadersWidth = 62;
+			gridTodo.Size = new Size(780, 469);
+			gridTodo.TabIndex = 0;
+			// 
+			// boxTodo
+			// 
+			boxTodo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			boxTodo.Location = new Point(12, 487);
+			boxTodo.Multiline = true;
+			boxTodo.Name = "boxTodo";
+			boxTodo.Size = new Size(780, 125);
+			boxTodo.TabIndex = 1;
+			boxTodo.TextChanged += boxTodo_TextChanged;
+			// 
+			// contextMenuStrip1
+			// 
+			contextMenuStrip1.ImageScalingSize = new Size(24, 24);
+			contextMenuStrip1.Name = "contextMenuStrip1";
+			contextMenuStrip1.Size = new Size(61, 4);
+			// 
+			// buttonSimpan
+			// 
+			buttonSimpan.BackColor = Color.LightGreen;
+			buttonSimpan.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			buttonSimpan.Location = new Point(12, 618);
+			buttonSimpan.Name = "buttonSimpan";
+			buttonSimpan.Size = new Size(780, 88);
+			buttonSimpan.TabIndex = 3;
+			buttonSimpan.Text = "SIMPAN";
+			buttonSimpan.UseVisualStyleBackColor = false;
+			buttonSimpan.Click += buttonSimpan_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(804, 718);
+			Controls.Add(buttonSimpan);
+			Controls.Add(boxTodo);
+			Controls.Add(gridTodo);
+			Name = "Form1";
+			Text = "TODO ASIK";
+			((System.ComponentModel.ISupportInitialize)gridTodo).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private DataGridView gridTodo;
+		private TextBox boxTodo;
+		private ContextMenuStrip contextMenuStrip1;
+		private Button buttonSimpan;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+using Npgsql;
+using System.Data;
+
+namespace WinformNPGSQL
+{
+	public partial class Form1 : Form
+	{
+		string InputTodo;
+		DatabaseHandler db = new DatabaseHandler();
+		public Form1()
+		{
+			InitializeComponent();
+			gridTodo.DataSource = db.GetTodo();
+		}
+
+		private void boxTodo_TextChanged(object sender, EventArgs e)
+		{
+			InputTodo = boxTodo.Text;
+		}
+
+		private void buttonSimpan_Click(object sender, EventArgs e)
+		{
+			db.AddTodo(InputTodo);
+			MessageBox.Show("Berhasil Simpan");
+			gridTodo.Rows.Clear();
+			db
+		}
+	}
+}
+
+
+public class DatabaseHandler
+{
+	NpgsqlConnection conn;
+
+	public DatabaseHandler()
+	{
+		conn = new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=pbo");
+		conn.Open();
+	}
+
+	public DataTable GetTodo()
+	{
+		DataTable dt = new DataTable();
+		using (var cmd = new NpgsqlCommand("SELECT id, teks FROM todo ORDER BY id", conn))
+		using (var reader = cmd.ExecuteReader())
+		{
+			dt.Load(reader);
+		}
+		return dt;
+	}
+
+	public void AddTodo(string teks)
+	{
+		using (var cmd = new NpgsqlCommand("INSERT INTO todo(teks) VALUES(@teks)", conn))
+		{
+			cmd.Parameters.AddWithValue("@teks", teks);
+			cmd.ExecuteNonQuery();
+		}
+	}
+
+	public void UpdateTodo(int id, string newText)
+	{
+		using (var cmd = new NpgsqlCommand("UPDATE todo SET teks = @teks WHERE id = @id", conn))
+		{
+			cmd.Parameters.AddWithValue("@id", id);
+			cmd.Parameters.AddWithValue("@teks", newText);
+			cmd.ExecuteNonQuery();
+		}
+	}
+
+
+	public void DeleteTodo(int id)
+	{
+		using (var cmd = new NpgsqlCommand("DELETE FROM todo WHERE id = @id", conn))
+		{
+			cmd.Parameters.AddWithValue("@id", id);
+			cmd.ExecuteNonQuery();
+		}
+	}
+
+
+	~DatabaseHandler()
+	{
+		conn.Close();
+	}
+
+}
